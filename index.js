17855a26abb75bb43714db4136fab5f7333cf8c5
Yu Watanabe <yuw27b@gmail.com>
2018-02-13 22:13:22 +0900

    Add styles

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ import xhrPool from './XhrPool.js';
 const requestButton = document.querySelectorAll('.request_button');
 const cancelButton = document.querySelectorAll('.request_cancel');
 const cancelAllButton = document.querySelector('.request_cancelAll');
-const requestListContainer = document.querySelector('.request_onGoing');
-const responseLogContainer = document.querySelector('.responseLog');
+const requestListContainer = document.querySelector('.pendingList_ul');
+const responseLogContainer = document.querySelector('.responseLog_ul');
+const clearButton = document.querySelector('.clear_button');
 
 requestButton.forEach(el => {
   el.addEventListener('click', (e) => {
@@ -35,7 +36,7 @@ cancelAllButton.addEventListener('click', () => {
 });
 
 const updateRequestList = () => {
-  document.querySelectorAll('.request_onGoing > li').forEach(el => {
+  document.querySelectorAll('.pendingList_ul > li').forEach(el => {
     el.remove();
   });
   const requestListItems = xhrPool.requestList().map(req => {
@@ -50,4 +51,10 @@ const addResponseLog = (res => {
   const el = document.createElement('li');
   el.append(res.timestamp);
   responseLogContainer.append(el);
+});
+
+clearButton.addEventListener('click', () => {
+  document.querySelectorAll('.responseLog_ul > li').forEach(el => {
+    el.remove();
+  });
 });
